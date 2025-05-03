ca2739aa524c9915282fa231a0aa31ee5d59b59f
Ananyashree K G <130287235+Ananyashreekg@users.noreply.github.com>
2025-05-03 16:43:45 +0000

    Successfully completed 3rd question

--- src/Module 3/Introduction to ReactJS/Ques_3.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_3.jsx
@@ -2,6 +2,7 @@
 // Description: Change name of the current functional component into Greeting that returns a welcoming message.
 // Use this component in App.js
 import React from 'react';
+import PropTypes from 'prop-types'; // Import PropTypes
 
 // Functional component that accepts a 'message' prop
 const Greeting = ({ message }) => {
@@ -10,6 +11,11 @@ const Greeting = ({ message }) => {
       <h1>{message || 'Welcome to React!'}</h1> {/* Default message if no prop is passed */}
     </div>
   );
+};
+
+// Prop type validation for 'message' prop
+Greeting.propTypes = {
+  message: PropTypes.string, // Ensures 'message' is a string
 };
 
 export default Greeting;
